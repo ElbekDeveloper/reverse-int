6f87eef69047f0c39c27cfea50ba3d671272ce4c
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-09-10 12:24:49 +0500

    solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return (n * Math.sign(n))
+        .toString()
+        .split("")
+        .reverse()
+        .map((x) => parseInt(x, 10))
+        .join("");
+};
